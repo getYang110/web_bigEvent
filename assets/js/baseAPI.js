a30f9544860d0b14714f7ada2dbe9f9a63675774
itheima <itheima@itcast.cn>
2020-11-24 16:42:53 +0800

    修改baseAPI

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,11 @@
 // 为页面上 所有基于 jq的ajax 请求发送之前，对参数对象进行处理
 $.ajaxPrefilter(function (Obj) {
     Obj.url = 'http://ajax.frontend.itheima.net' + Obj.url;
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (Obj.url.indexOf('/my/') > -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+    }
 })
